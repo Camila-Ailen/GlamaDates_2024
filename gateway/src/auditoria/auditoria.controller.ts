f426e60f245994324f4fe48c2ca36b16385d8655
Camila-Ailen <camila.gomez@fceqyn.unam.edu.ar>
2025-06-03 10:17:29 -0300

    auth para auditoria

--- gateway/src/auditoria/auditoria.controller.ts
+++ gateway/src/auditoria/auditoria.controller.ts
@@ -3,12 +3,14 @@ import { AuditoriaService } from './auditoria.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { AuditsPaginationDto } from './dto/pagination-audits.dto';
 import { ResposeDTO } from '@/base/dto/base.dto';
+import { Auth } from '@/auth/auth.decorator';
 
 @Controller('auditoria')
 export class AuditoriaController {
   constructor(private readonly auditoriaService: AuditoriaService) { }
 
   @Get()
+  @Auth('read:audits')
   @ApiOperation({ summary: 'Get all audit logs' })
   async all(@Query() query: AuditsPaginationDto): Promise<ResposeDTO> {
     const audits = await this.auditoriaService.all({ query });
@@ -16,6 +18,7 @@ export class AuditoriaController {
   }
 
   @Get('filter-options')
+  @Auth('read:audits')
   @ApiOperation({ summary: 'Get filter options for audits' })
   async getFilterOptions(): Promise<ResposeDTO> {
     const options = await this.auditoriaService.getFilterOptions();
